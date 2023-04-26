9cca5c2fda88912e4d1ec0fdedabb8bcc9c18765
Koulie <kouliecodes@gmail.com>
2023-04-26 15:31:06 -0400

    Finish the JS for the game

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // console.log("Hello world!");
-let options = ['rock', 'paper', 'scissors'];
+let options = ['Rock', 'Paper', 'Scissors'];
+let validOptions = ['rock', 'paper', 'scissors']
 
 function getCompChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
-    return options[randomIndex];
+    return validOptions[randomIndex];
 };
 
 // function processInput() {
@@ -24,5 +25,48 @@ function getUserInput() {
     return userChoice.toLowerCase();
 };
 
-console.log(getUserInput())
+let playerScore = 0
+let compScore = 0
+
+
+function printResults(comp, user) {
+    let winStatement = `You Win! You chose ${user} and the computer chose ${comp}.`;
+    let loseStatement = `You Lose! You chose ${user} and the computer chose ${comp}.`;
+    if (comp === user) {
+        console.log(`Tie! you both chose ${comp}`);
+    } else if (comp === 'rock' && user === 'paper') {
+        console.log(winStatement);
+        playerScore ++;
+    } else if (comp === 'rock' && user === 'scissors') {
+        console.log(loseStatement);
+        compScore ++;
+    } else if (comp === 'paper' && user === 'scissors') {
+        console.log(winStatement);
+        playerScore ++;
+    } else if (comp === 'paper' && user === 'rock') {
+        console.log(loseStatement);
+        compScore ++;
+    } else if (comp === 'scissors' && user === 'rock') {
+        console.log(winStatement);
+        playerScore ++;
+    } else if (comp === 'scissors' && user === 'paper') {
+        console.log(loseStatement);
+        compScore ++;
+    }
+}
+
+function playGame() {
+    let compAnswer = getCompChoice();
+    let userAnswer = getUserInput()
+    printResults(compAnswer, userAnswer)
+    
+}
+while (playerScore + compScore < 5) {
+    playGame()
+    console.log(playerScore)
+    console.log(compScore)
+}
+console.log(`The final score is ${playerScore} for you and ${compScore} for the computer!`)
+
+
 
